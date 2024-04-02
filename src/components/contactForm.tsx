5bde8c1f66169da11a5982af18eb6b16f932d90c
bruno <bruno@brunos-MacBook-Pro.local>
2024-04-02 15:32:22 -0300

    wip: contact form

---
+++ src/components/contactForm.tsx
@@ -0,0 +1,54 @@
+import { SyntheticEvent, useRef } from "react";
+import { z } from "zod";
+
+import { Input } from "@/components/ui/input";
+import { api } from "@/utils/api";
+
+const messageSchema = z.object({
+    sender: z.string().min(1).max(25),
+    email: z.string().min(1).max(50).email(),
+    message: z.string().min(1).max(250),
+})
+
+
+export function ContactForm() {
+    const nameRef = useRef<HTMLInputElement>(null)
+    const emailRef = useRef<HTMLInputElement>(null)
+    const messageRef = useRef<HTMLInputElement>(null)
+
+    const sendMessageMutation = api.contact.create.useMutation();
+
+    function onMessageSend(e: SyntheticEvent) {
+        e.preventDefault()
+
+        // faz a validação dos valores dos campos sem throw de erro
+        const res = messageSchema.safeParse({
+            sender: nameRef.current?.value || "",
+            email: emailRef.current?.value || "",
+            message: messageRef.current?.value || "",
+        })
+
+        if (res.success) {
+            sendMessageMutation.mutate({
+                sender: res.data.sender,
+                email: res.data.email,
+                message: res.data.message,
+            })
+            // TODO: toast com mensagem enviada
+        } else {
+            // TODO: mensagem de erro
+        }
+
+    }
+
+    return (
+        <form
+            className="w-4/6 max-w-xl flex flex-col gap-4 items-center"
+            onSubmit={onMessageSend}
+        >
+            <Input placeholder="Nome" ref={nameRef} />
+            <Input placeholder="Email" ref={emailRef} />
+            <Input placeholder="Mensagem" ref={messageRef} />
+        </form>
+    )
+}
