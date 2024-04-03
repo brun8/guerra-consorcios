847d351979d18546256285fe0b004cbf1aea6384
bruno <brunofvteixeira@gmail.com>
2024-04-02 21:41:20 -0300

    index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,9 +11,21 @@ export default function Home() {
         <meta name="description" content="" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex flex-col items-center xl:pt-44 pt-32">
-        <ContactForm />
-      </main>
+      <main className="flex flex-col ">
+        <section className="min-h-screen">
+        </section>
+        <section className="flex min-h-screen items-center">
+          <div className="flex-1 flex gap-28 max-w-7xl mx-auto">
+            <div className="hidden xl:flex flex-col gap-6">
+              <h2 className="text-5xl font-bold">Entre em contato</h2>
+              <p className="text-xl">
+                {/* TODO: algum texto aqui */}
+              </p>
+            </div>
+            <ContactForm />
+          </div>
+        </section>
+      </main >
     </>
   );
 }
